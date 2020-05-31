8ca4c336a4ea5ce78d55654c7d531417ab04c148
Juan Cerrutti <jicerrutti@gmail.com>
2020-05-31 17:47:30 -0300

    Updates

---
+++ precache-manifest.06824ed09498343798d3fc12f22b1b3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5330db03c1a1d869c12fb0e265fcc86d",
+    "url": "/another-reddit-clone-mobx/index.html"
+  },
+  {
+    "revision": "55587272743229ba401d",
+    "url": "/another-reddit-clone-mobx/static/css/main.2c36f4e1.chunk.css"
+  },
+  {
+    "revision": "d087fb0a5ccaade86e24",
+    "url": "/another-reddit-clone-mobx/static/js/2.51842bec.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/another-reddit-clone-mobx/static/js/2.51842bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55587272743229ba401d",
+    "url": "/another-reddit-clone-mobx/static/js/main.8b092f5d.chunk.js"
+  },
+  {
+    "revision": "3f2c7dd7f6e7ea80834f",
+    "url": "/another-reddit-clone-mobx/static/js/runtime-main.a96346be.js"
+  },
+  {
+    "revision": "4c471c209d7e0fd4e83120c9f84f0079",
+    "url": "/another-reddit-clone-mobx/static/media/comment.4c471c20.svg"
+  }
+]);
